72184222f254ee3570c44a7b68fa8e06d766a6d8
JohnnySyu <f87199443@gmail.com>
2020-10-29 21:14:45 +0800

    add count

---
+++ forumAPItest/forumAPItest/Controllers/CountbackController.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web.Http;
+
+namespace forumAPItest.Controllers
+{
+    public class CountbackController : ApiController
+    {
+        // GET: api/Countback
+        finaldbEntities1 db = new finaldbEntities1();
+        public JObject Get()
+        {
+            var fblogmemberCount = from p in db.blogBinding
+                             group p by p.memberdb.mb_employeeName into g
+                             select new
+                             {
+                                 member = g.Key,
+                                 Count = g.Count()
+                             };
+          
+                                 
+            var fmemberCount = from q in db.memberdb
+                               group q by q.mb_employeeGender into g
+                               select new
+                               {
+                                   gender = g.Key ,
+                                   Count = g.Count()
+                               };
+
+            var ffourmTypeCount = from q in db.forumMemberBinding
+                                  group q by q.forumType.Type into g
+                                  select new
+                                  {
+                                      Type = g.Key,
+                                      Count = g.Count()
+                                  };
+
+          
+
+           
+            
+            var result = new
+            {
+                fblogmemberCount,
+                fmemberCount,
+                ffourmTypeCount,
+                
+            };
+
+            string strJson = JsonConvert.SerializeObject(result, Formatting.Indented);
+            JObject o = JObject.Parse(strJson);
+            return o;
+        }
+
+        // GET: api/Countback/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Countback
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Countback/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Countback/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
